69afa6924e2fac505dedce9d968116cb37066bca
Ryan Bemis <ryanbemis40@gmail.com>
2026-02-21 12:48:34 -0500

    blurred background

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,8 +2,9 @@ import styles from "./styles.module.css";
 
 export default function Home() {
   return ( 
-    <div className={styles.wall}>
+    <div>
       <div className={styles.logo}>
+        <img src="https://as2.ftcdn.net/jpg/03/24/93/31/1000_F_324933141_WgPQPeuxUOW2RhXNZx6iTz9AyLFz2rKP.jpg" className={styles.wall}></img>
         <img src="/logo/sign.svg"></img>
       </div>
       <form action={`${process.env.NEXT_PUBLIC_URL}/join`}>
