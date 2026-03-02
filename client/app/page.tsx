7cdf1582716d2497aaa4b437b69d7cd456eca3c4
Ryan Bemis <ryanbemis40@gmail.com>
2026-03-02 11:30:33 -0500

    removed vertical divider on main page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -19,9 +19,6 @@ export default function Home() {
       <div className={styles.cardsWrapper}>
         <CreateGameCard />
 
-        {/* Vertical divider */}
-        <div className={styles.verticalDivider} />
-
         <JoinGameCard />
       </div>
 
